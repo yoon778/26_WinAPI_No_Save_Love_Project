77095a5bfa622dfc9111c3be1eeb8a4c66c19786
sanu1227 <rlatosn@tukorea.ac.kr>
2026-05-06 22:05:56 +0900

    feat: add ramen.h

--- No_Save_Love/No_Save_Love/ramen.h
+++ No_Save_Love/No_Save_Love/ramen.h
@@ -1,6 +1,18 @@
 #pragma once
 class ramen
 {
-	
+public:
+	bool water;
+	bool noodle;
+	bool soup;
+	bool egg;
+	bool cheese;
+	bool dumpling;
+
+
+	void clear();
+	void makebasicramen();
+	void makerandramen();
+	bool ramenSame(const ramen& other);
 };
 
